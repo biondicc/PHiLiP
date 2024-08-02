102b5069755b64c535a0f552bb723f97a17c229a
Calista Biondic <122407183+biondicc@users.noreply.github.com>
2024-08-02 14:57:43 -0400

    temporary change to solve HROM on large mesh

--- src/testing/rom_import_helper_functions.cpp
+++ src/testing/rom_import_helper_functions.cpp
@@ -89,7 +89,7 @@ bool getSnapshotParamsFromFile(Eigen::MatrixXd& snapshot_parameters, std::string
 void getROMPoints(Eigen::MatrixXd& rom_points, const Parameters::AllParameters *const all_parameters) {
     const double pi = atan(1.0) * 4.0;
     if(all_parameters->reduced_order_param.parameter_names.size() == 1){
-        rom_points.conservativeResize(200,1);
+        rom_points.conservativeResize(20,1);
         RowVectorXd parameter1_range;
         parameter1_range.resize(2);
         parameter1_range << all_parameters->reduced_order_param.parameter_min_values[0], all_parameters->reduced_order_param.parameter_max_values[0];
@@ -107,7 +107,7 @@ void getROMPoints(Eigen::MatrixXd& rom_points, const Parameters::AllParameters *
         }
     }
     else if(all_parameters->reduced_order_param.parameter_names.size() == 2){
-        rom_points.conservativeResize(400, 2);
+        rom_points.conservativeResize(260, 2);
         RowVectorXd parameter1_range;
         parameter1_range.resize(2);
         parameter1_range << all_parameters->reduced_order_param.parameter_min_values[0], all_parameters->reduced_order_param.parameter_max_values[0];
@@ -127,12 +127,14 @@ void getROMPoints(Eigen::MatrixXd& rom_points, const Parameters::AllParameters *
         int row = 0;
         for (int i = 0; i < 20; i++){
             for(int j = 0; j < 20; j++){
-                rom_points(row, 0) =  parameter1_range[0] + (step_1 * i);
-                rom_points(row, 1) =  parameter2_range[0] + (step_2 * j);
+                if (i > 6){
+                    rom_points(row, 0) =  parameter1_range[0] + (step_1 * i);
+                    rom_points(row, 1) =  parameter2_range[0] + (step_2 * j);
 
-                std::cout << rom_points(row, 0)  << std::endl;
-                std::cout << rom_points(row, 1)  << std::endl;
-                row ++;
+                    std::cout << rom_points(row, 0)  << std::endl;
+                    std::cout << rom_points(row, 1)  << std::endl;
+                    row ++;
+                }
             }
         }
     }
